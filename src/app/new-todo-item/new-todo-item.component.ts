8132165b855781b7f683e48f4948dbe883d17e51
Zach Dean <zdean3721@gmail.com>
2019-10-23 20:07:24 -0500

    new-todo-item component added

---
+++ src/app/new-todo-item/new-todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-todo-item',
+  templateUrl: './new-todo-item.component.html',
+  styleUrls: ['./new-todo-item.component.css']
+})
+export class NewTodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
