7940c1c72220ce17175523de7be691f3acfa269a
Zach Dean <zdean3721@gmail.com>
2019-10-23 20:08:39 -0500

    components added to app moudle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { NewTodoItemComponent } from './new-todo-item/new-todo-item.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    NewTodoItemComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
